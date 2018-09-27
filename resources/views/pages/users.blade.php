82e9849d4dc27693ca5a14aa9c5963bc84713bad
Albert Daniel <albertagoya@gmail.com>
2018-09-27 11:37:40 +0300

    3Coommit

--- resources/views/pages/users.blade.php
+++ resources/views/pages/users.blade.php
@@ -2,39 +2,50 @@
 
 @section('content')
 
-<table class="table table-hover">
+<div class="container">
+    <div class="card card-success">
+        <div class="card-header">
+                <h4>Users page</h4>
+        </div>
+        <div class="card-body">
+                <table class="table table-hover">
 
-    <thead>
+                        <thead>
+                    
+                          <th>Username</th>
+                    
+                          <th>Email</th>
+                    
+                          <th>Created On</th>
+                    
+                        </thead>
+                    
+                        <tbody>
+                    
+                            
+                        {{-- <p>{{$pagebody}}</p> --}}
+                    
+                    
+                    @foreach($users as $user)
+                    
+                            <tr>
+                    
+                              <td>{{$user->name}} </td>
+                    
+                              <td>{{$user->email}} </td>
+                    
+                              <td>{{$user->created_at}} </td>
+                    
+                    
+                            </tr>
+                    @endforeach
+                    
+                        </tbody>
+                    
+                    </table>
+        </div>
+    </div>
+</div>
 
-      <th>Username</th>
 
-      <th>Email</th>
-
-      <th>Created On</th>
-
-    </thead>
-
-    <tbody>
-
-        <h1>Users page</h1>
-    {{-- <p>{{$pagebody}}</p> --}}
-
-
-@foreach($users as $user)
-
-        <tr>
-
-          <td>{{$user->name}} </td>
-
-          <td>{{$user->email}} </td>
-
-          <td>{{$user->created_at}} </td>
-
-
-        </tr>
-@endforeach
-
-    </tbody>
-
-</table>
 @endsection
